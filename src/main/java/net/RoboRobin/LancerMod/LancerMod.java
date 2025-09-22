95bcf87fea2b8a45e8672f268a163da684b97841
Robin <83183447+Robo-Robin@users.noreply.github.com>
2025-09-22 10:13:20 -0500

    added first creative mode tabs

--- src/main/java/net/RoboRobin/LancerMod/LancerMod.java
+++ src/main/java/net/RoboRobin/LancerMod/LancerMod.java
@@ -1,6 +1,7 @@
 package net.RoboRobin.LancerMod;
 
 import net.RoboRobin.LancerMod.block.ModBlocks;
+import net.RoboRobin.LancerMod.item.ModCreativeModeTabs;
 import net.RoboRobin.LancerMod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -39,6 +40,8 @@ public class LancerMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/RoboRobin/LancerMod/item/ModCreativeModeTabs.java
@@ -0,0 +1,46 @@
+package net.RoboRobin.LancerMod.item;
+
+import net.RoboRobin.LancerMod.LancerMod;
+import net.RoboRobin.LancerMod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LancerMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> PRINTING_ITEMS_TAB  = CREATIVE_MODE_TAB.register("printing_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.FILAMENT.get()))
+                    .title(Component.translatable("creativetab.lancerintegration.printing_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.FILAMENT);
+                        output.accept(ModItems.STEEL_FILAMENT);
+                    })
+
+                    .build());
+
+    public static final Supplier<CreativeModeTab> BATTLEMAP_BLOCK_TAB  = CREATIVE_MODE_TAB.register("battlemap_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.HEXMAP_L_BLOCK))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(LancerMod.MOD_ID, "printing_items_tab"))
+                    .title(Component.translatable("creativetab.lancerintegration.battlemap_blocks"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.HEXMAP_L_BLOCK);
+                        output.accept(ModBlocks.HEXMAP_R_BLOCK);
+                    })
+
+                    .build());
+
+
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+
+}
